2b1623517643b0286daff293a844996b165360cf
youplaheij <youplaheij@icloud.com>
2025-06-10 14:57:18 +0200

    Updated the MapManager.cs and LaserScan.cs to be more compatible with the rest of the project

--- LaserScan.cs
+++ LaserScan.cs
@@ -1,48 +1,76 @@
 using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
 using RosMessageTypes.Sensor;
-using Unity.Robotics.UrdfImporter.Control;
-using Codice.Client.BaseCommands.WkStatus.Printers;
 
 namespace RosSharp.Control
 {
     public class LaserScan : MonoBehaviour
     {
+        public Transform robotBase;
+        public GameObject obstaclePrefab;
+
         private float[] ranges;
         private float angleStart;
         private float angleEnd;
         private float angleIncrement;
-        
-        private ROSConnection ros;
+        private float rangeMin;
+        private float rangeMax;
 
-        public GameObject obstacle;
+        private ROSConnection ros;
+        public MapManager mapManager;
 
         void Start()
         {
             ros = ROSConnection.GetOrCreateInstance();
-            ros.Subscribe<LaserScanMsg>("/scan", Scan);
+            ros.Subscribe<LaserScanMsg>("/scan", OnScanReceived);
+
+            if (robotBase == null)
+                robotBase = transform;
         }
 
-        void Scan(LaserScanMsg msg)
+        void OnScanReceived(LaserScanMsg msg)
         {
-            this.ranges = msg.ranges;
-            this.angleStart = msg.angle_min;
-            this.angleEnd = msg.angle_max;
-            this.angleIncrement = msg.angle_min;
+            ranges = msg.ranges;
+            angleStart = msg.angle_min;
+            angleEnd = msg.angle_max;
+            angleIncrement = msg.angle_increment;
+            rangeMin = msg.range_min;
+            rangeMax = msg.range_max;
+            mapManager.UpdateMap();
+        }
 
-            Vector3[] points = new Vector3[this.ranges.Length];
-            Vector3 pos = transform.position;
+        void Update()
+        {
+            if (ranges == null) return;
 
-            for (int i = 0; i < this.ranges.Length; i++)
+            Vector3 pos = robotBase.position;
+            Quaternion robotRotation = Quaternion.Euler(0, robotBase.eulerAngles.y, 0);
+
+            for (int i = 0; i < ranges.Length; i++)
             {
-                float angle = angleStart + angleIncrement * i;
-                Vector3 point = new Vector3(pos.x + ranges[i] * Mathf.Sin(angle), pos.y, pos.z + ranges[i] * Mathf.Cos(angle));
-                Instantiate(obstacle, point, Quaternion.Euler(0, angle, 0));
-                points[i] = point;
-                
-                //GameObject myGameObject = Instantiate(obstacle, point, Quaternion.Euler(0, angle, 0));
-                //Destroy(myGameObject, 2f);
+                float range = ranges[i];
+                if (range < rangeMin || range > rangeMax) continue;
+
+                float angleRad = angleStart + angleIncrement * i;
+
+                // Flip angle to match Unity left-handed system if mirrored
+                angleRad = -angleRad;
+
+                Vector3 direction = robotRotation * new Vector3(Mathf.Sin(angleRad), 0, Mathf.Cos(angleRad));
+                Vector3 hitPoint = pos + direction * range;
+
+                Debug.DrawLine(pos, hitPoint, Color.red, Time.deltaTime);
             }
         }
+
+        public float[] getRanges()
+        {
+            return ranges;
+        }
+        
+        public float getAngleStart(){return angleStart;}
+        public float getAngleEnd(){return angleEnd;}
+        public float getAngleIncrement(){return angleIncrement;}
+        public int getNumPoints(){return ranges.Length;}
     }
 }
