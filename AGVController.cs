947077c1c45fcc81697a2601904c639becba5505
anushaTUe <a.asthana@student.tue.nl>
2025-06-11 10:41:14 +0200

    some changes to make it hopefully work

--- AGVController.cs
+++ AGVController.cs
@@ -3,9 +3,6 @@ using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
 using RosMessageTypes.Geometry;
 using Unity.Robotics.UrdfImporter.Control;
-using Codice.Client.BaseCommands.WkStatus.Printers;
-using rcl_interfaces.msg;
-
 
 namespace RosSharp.Control
 {
@@ -31,20 +28,17 @@ namespace RosSharp.Control
         private float lastCmdReceived = 0f;
 
         ROSConnection ros;
-        private RotationDirection direction;
         private float rosLinear = 0f;
         private float rosAngular = 0f;
 
         private MapManager mapManager;
+        private int[,] map;
+        private int gridSize;
+        private float cellSize;
 
-        public int frontierCount = 0;
         private Vector3 currentGoal;
         private bool navigatingToGoal = false;
         private float goalTolerance = 0.05f;
-
-        private int[,] map;
-        private int gridSize = 100;
-        private float cellSize = 0.01f;
 
         void Start()
         {
@@ -52,77 +46,48 @@ namespace RosSharp.Control
             wA2 = wheel2.GetComponent<ArticulationBody>();
             SetParameters(wA1);
             SetParameters(wA2);
+
             ros = ROSConnection.GetOrCreateInstance();
             ros.Subscribe<TwistMsg>("cmd_vel", ReceiveROSCmd);
             ros.RegisterPublisher<TwistMsg>("cmd_vel");
+
             mapManager = FindObjectOfType<MapManager>();
+            map = mapManager.mapGrid;
+            gridSize = mapManager.gridSize;
+            cellSize = mapManager.cellSize;
+
+            InvokeRepeating("TrySetNextFrontierGoal", 2f, 1f); // call periodically
         }
 
         void ReceiveROSCmd(TwistMsg cmdVel)
         {
-            if (cmdVel.linear.x == 0 && cmdVel.angular.z == 0)
-            {
-                rosLinear = 0;
-                rosAngular = 0;
-            }
-
-            else if (cmdVel.linear.x < 0)
-            {
-                rosLinear = (float)(cmdVel.linear.x - 0.02);
-            }
-            else if (cmdVel.angular.z < 0)
-            {
-                rosAngular = (float)(cmdVel.angular.z - 0.18);
-            }
-            else if (cmdVel.linear.x > 0)
-            {
-                rosLinear = (float)(cmdVel.linear.x + 0.02);
-            }
-            else if (cmdVel.angular.z > 0)
-            {
-                rosAngular = (float)(cmdVel.angular.z + 0.18);
-            }
+            rosLinear = (float)cmdVel.linear.x;
+            rosAngular = (float)cmdVel.angular.z;
             lastCmdReceived = Time.time;
         }
 
         void FixedUpdate()
         {
-            //Move(new Vector3(-10, 0, 0));
-            /*
-            if (mode == ControlMode.Keyboard)
+            if (Time.time - lastCmdReceived > ROSTimeout)
             {
-                KeyBoardUpdate();
+                rosLinear = 0;
+                rosAngular = 0;
             }
-            else if (mode == ControlMode.ROS)
-            {
-                ROSUpdate();
-            }     
-            */
+            RobotInput(rosLinear, rosAngular);
 
-            if (!navigatingToGoal)
+            if (navigatingToGoal && HasReachedGoal(currentGoal))
             {
-                Vector3 nextFrontier;
-                if (FindNextFrontier(out nextFrontier))
-                {
-                    currentGoal = nextFrontier;
-                    navigatingToGoal = true;
-                    Move(currentGoal);
-                }
-                else
-                {
-                    Debug.Log("no frontiers left");
-                }
+                navigatingToGoal = false;
             }
-            else
+        }
+
+        void TrySetNextFrontierGoal()
+        {
+            if (!navigatingToGoal && FindNextFrontier(out Vector3 nextGoal))
             {
-                if (HasReachedGoal(currentGoal))
-                {
-                    navigatingToGoal = false;
-                }
-                else
-                {
-                    Move(currentGoal);
-                }
+                currentGoal = nextGoal;
+                navigatingToGoal = true;
+                Move(currentGoal);
             }
         }
 
@@ -134,71 +99,24 @@ namespace RosSharp.Control
             joint.xDrive = drive;
         }
 
-        private void SetSpeed(ArticulationBody joint, float wheelSpeed = float.NaN)
+        private void SetSpeed(ArticulationBody joint, float wheelSpeed)
         {
             ArticulationDrive drive = joint.xDrive;
-            drive.targetVelocity = float.IsNaN(wheelSpeed)
-                ? ((2 * maxLinearSpeed) / wheelRadius) * Mathf.Rad2Deg
-                : wheelSpeed;
+            drive.targetVelocity = wheelSpeed;
             joint.xDrive = drive;
         }
 
-        /*
         private void RobotInput(float speed, float rotSpeed)
         {
             speed = Mathf.Clamp(speed, -maxLinearSpeed, maxLinearSpeed);
             rotSpeed = Mathf.Clamp(rotSpeed, -maxRotationalSpeed, maxRotationalSpeed);
 
-            float wheel1Rotation = speed / wheelRadius;
-            float wheel2Rotation = wheel1Rotation;
-            float wheelSpeedDiff = (rotSpeed * trackWidth) / wheelRadius;
-
-            TwistMsg twist = new TwistMsg();
-            twist.linear.x = inputSpeed;
-            twist.angular.z = inputRotationSpeed;
-
-            ros.Publish("cmd_vel", twist);
-        }
-        */
-
-
-
-        private void ROSUpdate()
-        {
-
-            if (Time.time - lastCmdReceived > ROSTimeout)
-            {
-                rosLinear = 0f;
-                rosAngular = 0f;
-            }
-            Debug.Log("linear: " + rosLinear);
-            Debug.Log("angular: " + rosAngular);
-            RobotInput(rosLinear, -rosAngular);
-        }
-
-        private void RobotInput(float speed, float rotSpeed) // m/s and rad/s
-        {
-            if (speed > maxLinearSpeed)
-            {
-                speed = maxLinearSpeed;
-            }
-            if (rotSpeed > maxRotationalSpeed)
-            {
-                rotSpeed = maxRotationalSpeed;
-            }
             float wheel1Rotation = (speed / wheelRadius);
             float wheel2Rotation = wheel1Rotation;
             float wheelSpeedDiff = ((rotSpeed * trackWidth) / wheelRadius);
-            if (rotSpeed != 0)
-            {
-                wheel1Rotation = (wheel1Rotation + wheelSpeedDiff) * Mathf.Rad2Deg;
-                wheel2Rotation = (wheel2Rotation - wheelSpeedDiff) * Mathf.Rad2Deg;
-            }
-            else
-            {
-                wheel1Rotation *= Mathf.Rad2Deg;
-                wheel2Rotation *= Mathf.Rad2Deg;
-            }
+
+            wheel1Rotation = (wheel1Rotation + wheelSpeedDiff) * Mathf.Rad2Deg;
+            wheel2Rotation = (wheel2Rotation - wheelSpeedDiff) * Mathf.Rad2Deg;
 
             SetSpeed(wA1, wheel1Rotation);
             SetSpeed(wA2, wheel2Rotation);
@@ -207,104 +125,31 @@ namespace RosSharp.Control
         public void Move(Vector3 point)
         {
             Transform baseLink = transform.GetChild(0).GetChild(2);
-            float angle = Mathf.Atan2(point.x - baseLink.position.x, point.z - baseLink.position.z) * Mathf.Rad2Deg;
-            if (angle < 0) angle += 360;
+            Vector3 direction = (point - baseLink.position).normalized;
+            float angleToGoal = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
 
-            Debug.Log("Angle Before: " + angle);
-            if (point.x >= baseLink.position.x)
-            {
-                if (point.z < baseLink.position.z)
-                {
-                    angle = 180 - angle;
-                }
-            }
-            else if (point.x < baseLink.position.x)
-            {
-                if (point.z > baseLink.position.z)
-                {
-                    angle = 360 - angle;
-                }
-                else
-                {
-                    angle += 180;
-                }
-            }
-
-            Debug.Log("Angle: " + angle);
-            float baseAngle = baseLink.rotation.eulerAngles.y - 270;
-            if (baseAngle < 0) baseAngle += 360;
-
-            float angleDiff = angle - baseAngle;
-            if (angleDiff > 180) angleDiff -= 360;
-            if (angleDiff < -180) angleDiff += 360;
+            float currentAngle = baseLink.eulerAngles.y;
+            float angleDiff = Mathf.DeltaAngle(currentAngle, angleToGoal);
 
             TwistMsg twist = new TwistMsg();
-            if (Mathf.Abs(angleDiff) < 5)
-            {
-                Debug.Log("Forward");
+            if (Mathf.Abs(angleDiff) < 10f)
                 twist.linear.x = maxLinearSpeed;
-            }
-            else if (angleDiff > 0)
-            {
-                twist.angular.z = 0.75;
-            }
             else
-            {
-                twist.angular.z = -0.75;
-            }
+                twist.angular.z = Mathf.Sign(angleDiff) * maxRotationalSpeed;
+
             ros.Publish("cmd_vel", twist);
-            ReceiveROSCmd(twist);
-            RobotInput(rosLinear, rosAngular);
-        }
-
-        /// <summary>
-        /// 
-        /// this method determines if a given coordinate in a map is a frontier for the robot to
-        /// explore or not.
-        /// 
-        /// this method can work for basically any map. however, here this will only work for the 
-        /// occupation grid style map, where cells are labelled accordingly:
-        /// 1 = occupied, 0 = free, -1 = unknown
-        /// 
-        /// a frontier here is defined as a free cell (value 0 in this case) next to at least one
-        /// unexplored cell (value -1 in this case)
-        /// 
-        /// THIS METHOD MUST LOOP THROUGH ALL THE COORDINATES IN THE GIVEN MAP TO DETERMINE FRONTIERS.
-        /// IT WILL RETURN TRUE IF A COORDINATE IS INDEED A FRONTIER.
-        /// IN A LARGER FRONTIER ALGORITHM METHOD, IT CAN USE THIS METHOD, AND THEN USE PATHFINDING TO GET 
-        /// TO THE FRONTIER COORDINATE (IN THE SHORTEST AMOUNT OF TIME). 
-        /// 
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <returns></returns>
-        bool IsFrontier(int x, int y)
-        {
-            // getting the map grid from map manager class
-            map = mapManager.mapGrid;
-            if (map[x, y] != 0) return false;
-
-            if (x > 0 && map[x - 1, y] == -1) return true;
-            if (x < gridSize - 1 && map[x + 1, y] == -1) return true;
-            if (y > 0 && map[x, y - 1] == -1) return true;
-            if (y < gridSize - 1 && map[x, y + 1] == -1) return true;
-
-            // meets none of the cases above so cannot be a frontier
-            return false;
         }
 
         public bool FindNextFrontier(out Vector3 result)
         {
-            map = mapManager.mapGrid;
-            
-            for (int x = 0; x < gridSize; x++)
+            for (int x = 1; x < gridSize - 1; x++)
             {
-                for (int y = 0; y < gridSize; y++)
+                for (int y = 1; y < gridSize - 1; y++)
                 {
                     if (IsFrontier(x, y))
                     {
-                        result = new Vector3(x * cellSize, 0f, y * cellSize);
-                        frontierCount++;
+                        Vector3 worldPos = mapManager.GridToWorldPosition(x, y);
+                        result = new Vector3(worldPos.x, 0f, worldPos.z);
                         return true;
                     }
                 }
@@ -313,11 +158,18 @@ namespace RosSharp.Control
             return false;
         }
 
+        bool IsFrontier(int x, int y)
+        {
+            if (map[x, y] != 0) return false;
+            return map[x + 1, y] == -1 || map[x - 1, y] == -1 || map[x, y + 1] == -1 || map[x, y - 1] == -1;
+        }
+
         bool HasReachedGoal(Vector3 goal)
         {
-            Transform baseLink = transform.GetChild(0).GetChild(2);
-            Vector3 flatPos = new Vector3(baseLink.position.x, 0f, baseLink.position.z);
-            return Vector3.Distance(flatPos, goal) < goalTolerance;
+            Vector3 pos = transform.position;
+            pos.y = 0;
+            goal.y = 0;
+            return Vector3.Distance(pos, goal) < goalTolerance;
         }
     }
 }
--- LaserScanSensor.cs
+++ LaserScanSensor.cs
@@ -1,183 +1,56 @@
-using System;
 using System.Collections.Generic;
-using RosMessageTypes.Sensor;
-using RosMessageTypes.Std;
-using RosMessageTypes.BuiltinInterfaces;
-using Unity.Robotics.Core;
 using UnityEngine;
-using Unity.Robotics.ROSTCPConnector;
-using UnityEngine.Serialization;
 
 public class LaserScanSensor : MonoBehaviour
 {
-    public string topic;
-    [FormerlySerializedAs("TimeBetweenScansSeconds")]
-    public double PublishPeriodSeconds = 0.1;
-    public float RangeMetersMin = 0;
-    public float RangeMetersMax = 1000;
-    public float ScanAngleStartDegrees = -45;
-    public float ScanAngleEndDegrees = 45;
-    // Change the scan start and end by this amount after every publish
-    public float ScanOffsetAfterPublish = 0f;
-    public int NumMeasurementsPerScan = 10;
-    public float TimeBetweenMeasurementsSeconds = 0.01f;
-    public string LayerMaskName = "TurtleBot3Manual";
-    public string FrameId = "base_scan";
+    public int numRays = 360;
+    public float maxRange = 5f;
+    public float angleStart = 0f;
+    public float angleEnd = 360f;
+    public LayerMask obstacleLayer;
+    public bool showDebug = true;
 
-    float m_CurrentScanAngleStart;
-    float m_CurrentScanAngleEnd;
-    ROSConnection m_Ros;
-    double m_TimeNextScanSeconds = -1;
-    int m_NumMeasurementsTaken;
-    List<float> ranges = new List<float>();
+    private MapManager mapManager;
 
-    bool isScanning = false;
-    double m_TimeLastScanBeganSeconds = -1;
-
-    protected virtual void Start()
+    void Start()
     {
-        m_Ros = ROSConnection.GetOrCreateInstance();
-        m_Ros.RegisterPublisher<LaserScanMsg>(topic);
-
-        m_CurrentScanAngleStart = ScanAngleStartDegrees;
-        m_CurrentScanAngleEnd = ScanAngleEndDegrees;
-
-        m_TimeNextScanSeconds = Clock.Now + PublishPeriodSeconds;
-    }
-
-    void BeginScan()
-    {
-        isScanning = true;
-        m_TimeLastScanBeganSeconds = Clock.Now;
-        m_TimeNextScanSeconds = m_TimeLastScanBeganSeconds + PublishPeriodSeconds;
-        m_NumMeasurementsTaken = 0;
-    }
-
-    public void EndScan()
-    {
-        if (ranges.Count == 0)
+        mapManager = FindObjectOfType<MapManager>();
+        if (mapManager == null)
         {
-            Debug.LogWarning($"Took {m_NumMeasurementsTaken} measurements but found no valid ranges");
-        }
-        else if (ranges.Count != m_NumMeasurementsTaken || ranges.Count != NumMeasurementsPerScan)
-        {
-            Debug.LogWarning($"Expected {NumMeasurementsPerScan} measurements. Actually took {m_NumMeasurementsTaken}" +
-                             $"and recorded {ranges.Count} ranges.");
-        }
-
-        var timestamp = new TimeStamp(Clock.time);
-        // Invert the angle ranges when going from Unity to ROS
-        var angleStartRos = -m_CurrentScanAngleStart * Mathf.Deg2Rad;
-        var angleEndRos = -m_CurrentScanAngleEnd * Mathf.Deg2Rad;
-        if (angleStartRos > angleEndRos)
-        {
-            Debug.LogWarning("LaserScan was performed in a clockwise direction but ROS expects a counter-clockwise scan, flipping the ranges...");
-            var temp = angleEndRos;
-            angleEndRos = angleStartRos;
-            angleStartRos = temp;
-            ranges.Reverse();
-        }
-
-        var msg = new LaserScanMsg
-        {
-            header = new HeaderMsg
-            {
-                frame_id = FrameId,
-                stamp = new TimeMsg
-                {
-                    sec = timestamp.Seconds,
-                    nanosec = timestamp.NanoSeconds,
-                }
-            },
-            range_min = RangeMetersMin,
-            range_max = RangeMetersMax,
-            angle_min = angleStartRos,
-            angle_max = angleEndRos,
-            angle_increment = (angleEndRos - angleStartRos) / NumMeasurementsPerScan,
-            time_increment = TimeBetweenMeasurementsSeconds,
-            scan_time = (float)PublishPeriodSeconds,
-            intensities = new float[ranges.Count],
-            ranges = ranges.ToArray(),
-        };
-
-        m_Ros.Publish(topic, msg);
-
-        m_NumMeasurementsTaken = 0;
-        ranges.Clear();
-        isScanning = false;
-        var now = (float)Clock.time;
-        if (now > m_TimeNextScanSeconds)
-        {
-            Debug.LogWarning($"Failed to complete scan started at {m_TimeLastScanBeganSeconds:F} before next scan was " +
-                             $"scheduled to start: {m_TimeNextScanSeconds:F}, rescheduling to now ({now:F})");
-            m_TimeNextScanSeconds = now;
-        }
-
-        m_CurrentScanAngleStart += ScanOffsetAfterPublish;
-        m_CurrentScanAngleEnd += ScanOffsetAfterPublish;
-        if (m_CurrentScanAngleStart > 360f || m_CurrentScanAngleEnd > 360f)
-        {
-            m_CurrentScanAngleStart -= 360f;
-            m_CurrentScanAngleEnd -= 360f;
+            Debug.LogError("MapManager not found in the scene.");
         }
     }
 
-    public void Update()
+    void Update()
     {
-        if (!isScanning)
+        PerformScan();
+    }
+
+    void PerformScan()
+    {
+        float angleIncrement = (angleEnd - angleStart) / numRays;
+        Vector3 origin = transform.position;
+
+        for (int i = 0; i < numRays; i++)
         {
-            if (Clock.NowTimeInSeconds < m_TimeNextScanSeconds)
+            float angle = Mathf.Repeat(angleStart + i * angleIncrement, 360f);
+            Vector3 direction = Quaternion.Euler(0, angle, 0) * transform.forward;
+
+            if (Physics.Raycast(origin, direction, out RaycastHit hit, maxRange, obstacleLayer))
             {
-                return;
-            }
+                if (showDebug)
+                    Debug.DrawLine(origin, hit.point, Color.red);
 
-            BeginScan();
-        }
-
-
-        var measurementsSoFar = TimeBetweenMeasurementsSeconds == 0 ? NumMeasurementsPerScan :
-            1 + Mathf.FloorToInt((float)(Clock.time - m_TimeLastScanBeganSeconds) / TimeBetweenMeasurementsSeconds);
-        if (measurementsSoFar > NumMeasurementsPerScan)
-            measurementsSoFar = NumMeasurementsPerScan;
-
-        var yawBaseDegrees = transform.rotation.eulerAngles.y;
-        while (m_NumMeasurementsTaken < measurementsSoFar)
-        {
-            var t = m_NumMeasurementsTaken / (float)NumMeasurementsPerScan;
-            var yawSensorDegrees = Mathf.Lerp(m_CurrentScanAngleStart, m_CurrentScanAngleEnd, t);
-            var yawDegrees = yawBaseDegrees + yawSensorDegrees;
-            var directionVector = Quaternion.Euler(0f, yawDegrees, 0f) * Vector3.forward;
-            var measurementStart = RangeMetersMin * directionVector + transform.position;
-            var measurementRay = new Ray(measurementStart, directionVector);
-            var foundValidMeasurement = Physics.Raycast(measurementRay, out var hit, RangeMetersMax);
-            // Only record measurement if it's within the sensor's operating range
-            if (foundValidMeasurement)
-            {
-                ranges.Add(hit.distance);
+                mapManager?.UpdateMap(origin, hit.point, true);
             }
             else
             {
-                ranges.Add(float.MaxValue);
+                Vector3 missPoint = origin + direction * maxRange;
+                if (showDebug)
+                    Debug.DrawLine(origin, missPoint, Color.green);
+
+                mapManager?.UpdateMap(origin, missPoint, false);
             }
-
-            // Even if Raycast didn't find a valid hit, we still count it as a measurement
-            ++m_NumMeasurementsTaken;
         }
-
-        if (m_NumMeasurementsTaken >= NumMeasurementsPerScan)
-        {
-            if (m_NumMeasurementsTaken > NumMeasurementsPerScan)
-            {
-                Debug.LogError($"LaserScan has {m_NumMeasurementsTaken} measurements but we expected {NumMeasurementsPerScan}");
-            }
-            EndScan();
-        }
-
-    }
-    
-    public float[] GetRanges()
-    {
-        rangesArray = ranges.ToArray();
-        return rangesArray;
     }
 }
